5083de07acf01c248b824f253f636cbe53013d85
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2022-11-06 21:30:43 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -2,27 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-
-
-// Напиши скрипт створення і очищення колекції елементів. 
-// Користувач вводить кількість елементів в input і натискає кнопку Створити,
-// після чого рендериться колекція.Натисненням на кнопку Очистити, колекція елементів очищається.
-// Створи функцію createBoxes(amount), яка приймає один параметр - число.Функція створює стільки < div >,
-//   скільки вказано в amount і додає їх у div#boxes.
-
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX.
-
-{/* <div id="controls">
-  <input type="number" min="1" max="100" step="1" />
-  <button type="button" data-create>Create</button>
-  <button type="button" data-destroy>Destroy</button>
-</div> */}
-
-
-///// 10 задача ще в процесі...
-
 const boxes = document.querySelector("#boxes");
 const input = document.querySelector("input");
 
@@ -38,7 +17,7 @@ function onCreateBtnClick(event) {
 }
 
 function createBoxes(amount) {
-
+  boxes.textContent = "";
   amount = input.value;
 
   for (let i = 0; i < amount; i++){
@@ -52,6 +31,10 @@ function createBoxes(amount) {
  }
 
 function destroyBoxes() {
-  const boxAll = document.querySelectorAll(".newbox");
-  boxes.children.remove();
-  }
+  boxes.textContent = "";
+}
+  
+
+// удаление div по классу, работает только удаляя один
+//  const boxAll = document.querySelectorAll(".newbox");
+//   boxAll.remove();
